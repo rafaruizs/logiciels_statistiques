037490dfc6f9f7f6b43491f3dc707e964e39046a
Kassim Machioudi <kassim@machioudi.com>
2014-10-10 22:45:13 -0400

    Rencontre du 10 Oct.

--- server.R
+++ server.R
@@ -1,7 +1,16 @@
 
 #install.packages("shiny")
+#install.packages("maptools")
+#install.packages("ggplot2")
+#install.packages("RColorBrewer")
 
 library(shiny)
+library(ggplot2)
+library(proto)
+library(maptools)
+library(RColorBrewer)
+
+map <- readShapePoly(paste(getwd(), "/www/map/LIMADMIN.shp", sep=""))
 
 getColumnList <- function(data) {
   
@@ -9,7 +18,8 @@ getColumnList <- function(data) {
     return()
   }
   
-  return(as.list(names(data)[-1]))
+  #return(as.list(names(data)[-1]))
+  return(sapply(names(data), function(x) { return(class(data[[x]]))}, simplify=F))
   
 }
 
@@ -28,24 +38,72 @@ shinyServer(function(input, output) {
     return(dataset)
     
   })
+  
+  Colonnes <- reactive({
+    return(getColumnList(Data()))
+  })
 
   output$columns <- renderUI({
     
     column_list <- getColumnList(Data())
     
-    if (!is.null(column_list)) {
-      checkboxGroupInput('columns', 'Colonnes', column_list)
+    #column_with_types <- sapply(names(column_list), function(x) { paste(x, column_list[[x]]) })
+    if (!is.null(column_list)) {  
+      
+      i <- 1
+      column_with_types <- list()
+      name_list <- c()
+      for (c in names(column_list)) {
+        label <- paste(c, ' (', column_list[[c]], ')', sep='')
+        name_list <- c(name_list, label)
+        column_with_types <- c(column_with_types, i) 
+        i <- i + 1
+      }
+      names(column_with_types) <- as.list(name_list)
+      checkboxGroupInput('columns', 'Colonnes', column_with_types)
     }
     else {
       return()
     }
   })
   
-  output$contents <- renderTable({
+  output$colAgglo <- renderUI({
     
-    if (is.null(Data())) return(NULL)
-    data.frame(Data())
+    selectInput("colAgglo","Colonne Agglomeration", names(Colonnes()))
     
   })
+  
+  output$contents <- renderPlot({
+    
+    var <- input$columns 
+    
+    
+    if(is.null(var)) return(NULL)
+    
+    dt <- Data()
+    col <- names(Colonnes())[var]
+    
+    ds <- dt[[col]]
+    
+    maxy <- max(ds)
+    chunk <- (maxy-min(ds))/4
+    breaks <- c(0, min(ds)+chunk, min(ds)+2*chunk, min(ds)+3*chunk, maxy)
+    
+    plotvar <- unlist(dt[[col]])
+    nclr <- 9
+    plotclr <- brewer.pal(nclr, "Reds")
+    fillRed <- colorRampPalette(plotclr)
+    plotvar[plotvar >= maxy] <- maxy -1
+    colcode <- fillRed(maxy)[round(plotvar) + 1]
+    plot(map, col = colcode, border="dark grey", axes=F, las=1)
+    
+  })
+  
+  #output$contents <- renderTable({
+    
+    #if (is.null(Data())) return(NULL)
+    #data.frame(Data())
+    
+  #})
   
 })
--- ui.R
+++ ui.R
@@ -14,14 +14,17 @@ shinyUI(fluidPage(
       fileInput('file1', 'Choisir votre ensemble de données',
                 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
       
-      uiOutput("columns")
+      uiOutput("columns"),
+      
+      # variable reprsentant l'agglomeration
+      uiOutput("colAgglo")
       
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
       textOutput("message"),
-      tableOutput("contents")
+      plotOutput("contents")
     )
   )
 ))
